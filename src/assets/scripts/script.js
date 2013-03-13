a5a9cf669047778a7ac3f7d40482cfb3cedc6c63
Erwann MEST <erwann.mest@gmail.com>
2013-03-14 00:18:42 +0100

    Some fixes.

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,1 +1,1 @@
-// Leave my or put some JavaScript in here (jshint need some content in the file or it will fail :)
+// Leave me or put some JavaScript here (jshint needs some content in the file or it will fail :)
