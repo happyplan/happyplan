0510f7a940f7c8cce7f1c5668d0f0de67dd9afe3
Erwann MEST <erwann.mest@gmail.com>
2013-03-31 22:50:59 +0200

    Update publish.

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
-grunt dist
-git add .
-git commit -am "Source updated."
-git branch -D gh-pages
-git checkout -b gh-pages
-mv .gitignore .gitignore.tmp
-git rm -rqf .
-mv .gitignore.tmp .gitignore
-cp -r dist/* .
-git add .
-git rm .gitignore
-git commit -m "Site updated."
-git push -f --set-upstream origin gh-pages
-git checkout src
-git push --set-upstream origin src
+PUBLISH_BRANCH="gh-pages"
+if [ $1 ]
+  then
+    if [ $1 = "-test" ]
+      then
+        PUBLISH_BRANCH="test"
+    fi
+fi
+git checkout -b $PUBLISH_BRANCH
+# grunt dist
+# git add .
+# git commit -am "Source updated."
+# git branch -D $PUBLISH_BRANCH
+# git checkout -b $PUBLISH_BRANCH
+# mv .gitignore .gitignore.tmp
+# git rm -rqf .
+# mv .gitignore.tmp .gitignore
+# cp -r dist/* .
+# git add .
+# git rm .gitignore
+# git commit -m "Site updated."
+# git push -f --set-upstream origin $PUBLISH_BRANCH
+# git checkout src
+# git push --set-upstream origin src
