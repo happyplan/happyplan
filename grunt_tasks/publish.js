69f37e9c3871fbdab262915af4bb8f96ee3cf4a6
Maxime Thirouin <m@moox.io>
2014-01-12 06:22:42 +0100

    Add grunt-notify
    This include a custom message for default & publish command
    Close #130

--- grunt_tasks/publish.js
+++ grunt_tasks/publish.js
@@ -2,8 +2,9 @@ module.exports = function(grunt) {
   "use strict";
 
   grunt.registerTask('happyplan:publish', "Publish your website", [
-    'happyplan:dist',
-    'gh-pages'
+      'happyplan:dist'
+    , 'gh-pages'
+    , 'notify:publish'
   ]
     //function() {
       // @todo
